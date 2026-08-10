99d40fbcec53518de2a0bdb6dd8e0bcb6ef8ae18
mahammadm938 <shaikmahammad893@gmail.com>
2026-08-10 16:19:34 +0530

    restart

--- main.py
+++ main.py
@@ -1,18 +1,2 @@
 print("Hello world")
 
-# Create Cosmos DB client
-cosmos_client = CosmosClient(
-    COSMOS_ENDPOINT,
-    COSMOS_KEY
-)
-
-# Get database
-database = cosmos_client.get_database_client(
-    COSMOS_DATABASE
-)
-
-# Get container
-container = database.get_container_client(
-    COSMOS_CONTAINER
-)
-
