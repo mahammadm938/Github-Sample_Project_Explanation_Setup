e34e17cc91209eb975793ff9c04e07546624fe94
mahammadm938 <shaikmahammad893@gmail.com>
2026-08-10 16:30:05 +0530

    added azure cosmos db

--- main.py
+++ main.py
@@ -1,2 +1,29 @@
 print("Hello world")
 
+import os
+from azure.cosmos import CosmosClient
+from dotenv import load_dotenv
+
+load_dotenv()
+
+COSMOS_ENDPOINT = os.getenv("COSMOS_ENDPOINT")
+COSMOS_KEY = os.getenv("COSMOS_KEY")
+
+client = CosmosClient(COSMOS_ENDPOINT, COSMOS_KEY)
+
+database = client.get_database_client("PracticeDB")
+container = database.get_container_client("Users")
+
+
+name = input("Enter your name: ")
+
+item = {
+    "id": name,
+    "name": name
+}
+
+container.upsert_item(item)
+
+print(f"Hello {name}!")
+print("Welcome to Python World")
+print("Data saved to Cosmos DB!")
